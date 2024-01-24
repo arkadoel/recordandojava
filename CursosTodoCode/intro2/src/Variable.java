308d581e4aa400f86339645fb751a17dcddc42bd
FMinguela <fer.d.minguela@gmail.com>
2024-01-24 21:17:01 +0100

    comenzando

---
+++ CursosTodoCode/intro2/src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        int numero = 5;
+        String nombre = "Pedro";
+        char letra = 'a';
+        double temperatura = 38.5d;
+        boolean isOk = false;
+        long largo = 1123456789;
+        System.out.println("numero " + numero);
+        System.out.println("letra " + letra);
+        System.out.println("temperatura " + temperatura);
+        System.out.println("is ok " + isOk);
+        System.out.println("largo " + largo);
+    }
+}
